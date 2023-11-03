d178fbaa518f5a9585e1eeb968a0c5150a3c00a7
Nick8449 <ovchynnik.mykola@gmail.com>
2023-11-03 13:03:28 +0100

    add/task2_video
    time of the video saved to LS and set when updated page

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe);
+
+function saveTimeToLocalStorage(time) {
+  localStorage.setItem('videoplayer-current-time', time);
+}
+
+function restoreTimeFromLocalStorage() {
+  return localStorage.getItem('videoplayer-current-time') ?? 0;
+}
+
+player.on(
+  'timeupdate',
+  throttle(data => {
+    saveTimeToLocalStorage(data.seconds);
+  }, 1000) // save video timecode to Local Storage every second
+);
+
+player.setCurrentTime(restoreTimeFromLocalStorage());
