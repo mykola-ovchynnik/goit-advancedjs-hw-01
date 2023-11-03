faafac65bf3ec899ba007613fee21d17503a0d2c
Nick8449 <ovchynnik.mykola@gmail.com>
2023-11-03 12:04:46 +0100

    add/task1_gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+
+createMarkup(galleryItems, galleryList);
+
+function createMarkup(galleryItems, targetElement) {
+  const markup = galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                alt="${description}"
+            />
+        </a>
+        </li>`
+    )
+    .join('');
+
+  targetElement.insertAdjacentHTML('afterbegin', markup);
+}
+
+const gallery = new SimpleLightbox('.gallery a', {
+  captionSelector: 'img',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
