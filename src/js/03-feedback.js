20513738217d592c0b53f0976293774229d6fe5c
Nick8449 <ovchynnik.mykola@gmail.com>
2023-11-04 21:13:40 +0100

    add/task3-feedback-form

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+
+function handlerFormInput(event) {
+  if (event.target.name === 'email' || event.target.name === 'message') {
+    const formData = {
+      email: form.elements.email.value,
+      message: form.elements.message.value,
+    };
+
+    saveFormData(formData);
+  }
+}
+
+function saveFormData(formData) {
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+function restoreFormData() {
+  const restoredFormData = localStorage.getItem('feedback-form-state');
+  if (restoredFormData) {
+    const { email, message } = JSON.parse(restoredFormData);
+    form.elements.email.value = email;
+    form.elements.message.value = message;
+  }
+}
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  localStorage.removeItem('feedback-form-state');
+
+  form.reset();
+}
+
+restoreFormData();
+
+form.addEventListener('input', throttle(handlerFormInput, 500));
+form.addEventListener('submit', handleFormSubmit);
